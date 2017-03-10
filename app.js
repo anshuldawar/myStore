9db6d66c6f96d797f791c66d6b510cef2fcaa5b1
anshuldawar <anshuldawar1995@gmail.com>
2017-03-10 06:10:49 +0530

    added_product-tabs.html

--- app.js
+++ app.js
@@ -34,11 +34,25 @@ var app = angular.module('store',[]);
     };
   });
     
-    app.directive('productTabs', function() {
-    return {
-    restrict: 'E'
+     app.directive('productTabs', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'product-tabs.html',
+    controller: function() {
+      this.tab = 1;
+
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
+
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: 'tab'
   };
 });
+
     
     
     var gems = [
